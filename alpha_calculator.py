8bc01fc375a3c8cbe6c944795af32406b3b1086f
hanj99 <hanj99@gmail.com>
2014-03-08 13:24:18 +0900

    Update alpha_calculator.py

--- alpha_calculator.py
+++ alpha_calculator.py
@@ -1,12 +1,12 @@
-/*
-  *  Copyright (c) 2014, Joonhee Han.
- *
- *  This file is part of MealyHMM.
- *  MealyHMM is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
-*/
+'''
+   Copyright (c) 2014, Joonhee Han.
+ 
+   This file is part of MealyHMM.
+   MealyHMM is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+'''
 
 import numpy
 from Queue import Queue
